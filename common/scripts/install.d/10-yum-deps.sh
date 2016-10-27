7bae4784a1f823aaeb1423ee7194d7d1da15f3ec
g-2 E989 <newg2@g2field.(none)>
2016-10-27 12:04:27 -0700

    Fixes to install scripts.

--- common/scripts/install.d/10-yum-deps.sh
+++ common/scripts/install.d/10-yum-deps.sh
@@ -6,4 +6,5 @@ yum -y install yum-plugin-priorities epel-release osg-oasis \
     openssh-clients openssh-server libusb1-devel mysql-devel \
     python-devel libpng-devel qt-devel PyQt4 tkinter cmake lapack blas \
     libsodium-devel libXpm-devel devtoolset-2-gcc devtoolset-2-gcc-c++ \
-    devtoolset-2-binutils-devel fftw-devel unixODBC sqlite-devel
+    devtoolset-2-binutils-devel fftw-devel unixODBC sqlite-devel \
+    armadillo-devel
