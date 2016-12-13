1d3b23e5aec2160f53e1e70260633152c2d7b45c
Ran Hong <hongran@uw.edu>
2016-12-13 14:50:10 -0600

    adding custom page

---
+++ common/scripts/odb.d/14-custompage.cmd
@@ -0,0 +1,8 @@
+mkdir "/Custom"
+cd "/Custom"
+create "Absolute Probe Control&"[1][256]
+create "Path"[1][256]
+
+set "Absolute Probe Control&" "ControlPage.html"
+set "Path" "/Users/rhong/gm2software/TestMagnetFe/online/custom"
+
