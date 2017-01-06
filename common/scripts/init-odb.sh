9559bfc41e80ea127a21e8eb241a153f7094265d
Matthias W. Smith <mwsmith2@uw.edu>
2017-01-06 15:36:24 -0800

    Add call to initalize size of ODB and create missing dirs.

--- common/scripts/init-odb.sh
+++ common/scripts/init-odb.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Make sure the ODB is initialized.
+odbedit -e g2-field -s 20000000 -c 'clean'
+
+# Run all ODB subtree init scripts.
 cd $(dirname $(readlink -f $0))/odb.d
 
 for script in `ls [0-9]?-*.cmd`
--- common/scripts/odb.d/00-init-odb.cmd
+++ common/scripts/odb.d/00-init-odb.cmd
@@ -1,5 +1,10 @@
-set "Logger/ODB Dump" y
+create STRING "Experiment/Menu Buttons"
 set "Experiment/Menu Buttons" "Status, ODB, Messages, Alarms, Programs, History, MSCB, Help"
 create BOOL "/Experiment/Run Parameters/Root Output"
 set "/Experiment/Run Parameters/Root Output" n
+
+mkdir "Logger"
+create BOOL "/Logger/ODB Dump"
+set "Logger/ODB Dump" y
+
 mkdir "Settings"
