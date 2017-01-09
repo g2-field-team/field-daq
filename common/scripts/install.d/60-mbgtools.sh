9264c9068f1430dba224fba912b4b8262876c3f2
Matthias W. Smith <mwsmith2@uw.edu>
2017-01-09 10:50:00 -0800

    Adds cleanup line to mbgtools install.

--- common/scripts/install.d/60-mbgtools.sh
+++ common/scripts/install.d/60-mbgtools.sh
@@ -6,6 +6,7 @@ cd /usr/local/opt
 wget https://www.meinbergglobal.com/download/drivers/mbgtools-lx-3.4.0.tar.gz
 
 tar xf mbgtools-lx-3.4.0.tar.gz
+rm mbgtools-lx-3.4.0.tar.gz
 cd mbgtools-lx-3.4.0
 
 make clean
