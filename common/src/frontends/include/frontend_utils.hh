6eddbf05cfca28a7a64747c99d24a06176e89268
Matthias W. Smith <mwsmith2@uw.edu>
2017-01-09 14:02:56 -0800

    Moves mbg parse function into frontend_utils.hh

--- common/src/frontends/include/frontend_utils.hh
+++ common/src/frontends/include/frontend_utils.hh
@@ -12,11 +12,17 @@ about:  Contains helper functions for MIDAS frontends.
 \*===========================================================================*/
 
 //--- std includes ----------------------------------------------------------//
+#include <cstdarg>
+#include <cstdio>
+#include <iostream>
+#include <sstream>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <memory>
 #include <vector>
 #include <array>
 #include <mutex>
-#include <cstdarg>
-#include <iostream>
 #include <sys/time.h>
 
 //--- other includes --------------------------------------------------------//
@@ -84,6 +90,78 @@ inline int load_settings(char *frontend, boost::property_tree::ptree& conf)
   }
 
   return SUCCESS;
+}
+
+// Call a shell command and return stdout.
+inline std::string exec(const char *cmd) 
+{
+  char buffer[128];
+  std::string result("");
+  std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+  
+  if (!pipe) throw std::runtime_error("popen() failed!");
+
+  while (!feof(pipe.get())) {
+    if (fgets(buffer, 128, pipe.get()) != NULL) {
+      result += buffer;
+    }
+  }
+  
+  return result;
+}
+
+// Parse the output of mbgfasttstamp into UTC nanoseconds.
+inline uint64_t parse_mbg_string_ns(std::string ts) 
+{
+  std::stringstream ss;
+  std::string timestamp;
+  std::string str;
+  
+  bool valid_ts = false;
+  uint64_t time_ns = 0;
+  
+  // Push the input string into stringstream.
+  ss.str(ts);
+
+  while (ss.good()) {
+
+    std::getline(ss, str, '\n');
+
+    if (str.find("HR time raw:") == 0) {
+      valid_ts = true;
+      timestamp = str;
+      break;
+    }
+  }
+
+  if (valid_ts) {
+    
+    ss.str(timestamp);
+    uint64_t t;
+
+    std::getline(ss, str, ':');
+
+    std::getline(ss, str, ',');
+    ss.str(str);
+
+    std::string sec_string;
+    std::string nsec_string;
+    
+    std::getline(ss, sec_string, '.');
+    std::getline(ss, nsec_string, '.');
+    
+    t = std::stoul(sec_string, nullptr, 16);
+    time_ns += t * 1.0e9;
+
+    t = std::stoul(nsec_string, nullptr, 16);
+    time_ns += 1.0e9 * (t / 4294967296.0); // 1 = fraction of int max.
+
+  } else {
+
+    std::cout << "No valid timestamp found." << std::endl;
+  }
+
+  return time_ns;
 }
 
 #endif
--- common/tests/mbgtest/mbg-timestamp-test.cxx
+++ common/tests/mbgtest/mbg-timestamp-test.cxx
@@ -1,66 +1,4 @@
-#include <cstdio>
-#include <iostream>
-#include <sstream>
-#include <memory>
-#include <stdexcept>
-#include <string>
-#include <vector>
-
-std::string exec(const char *cmd) 
-{
-  char buffer[128];
-  std::string result("");
-  std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
-  
-  if (!pipe) throw std::runtime_error("popen() failed!");
-
-  while (!feof(pipe.get())) {
-    if (fgets(buffer, 128, pipe.get()) != NULL) {
-      result += buffer;
-    }
-  }
-  
-  return result;
-}
-
-uint64_t parse_mbg_string_us(std::string ts) 
-{
-  std::stringstream ss;
-  std::string timestamp;
-  std::string str;
-  ss.str(ts);
-
-  bool valid_ts = false;
-
-  while (!ss.good()) {
-    std::getline(ss, str);
-
-    if (str.find("HR time raw:") == 0) {
-      valid_ts = true;
-      timestamp = str;
-      break;
-    }
-  }
-
-  if (valid_ts) {
-    
-    ss.str(timestamp);
-
-    std::getline(ss, str, ':');
-    std::cout << str << std::endl;
-
-    std::getline(ss, str, ',');
-    std::cout << str << std::endl;
-
-    std::getline(ss, str, ',');
-    std::cout << str << std::endl;
-  } else {
-    std::cout << "No valid timestamp found." << std::endl;
-  }
-
-  return 0;
-}
-
+#include "frontend_utils.hh"
 
 int main(int argc, char* argv[])
 {
@@ -71,8 +9,8 @@ int main(int argc, char* argv[])
   }
 
   for (auto s : outvec) {
-    //    std::cout << s << std::endl;
-    parse_mbg_string_us(s);
+    std::cout << s << std::endl;
+    std::cout << parse_mbg_string_ns(s) * 1.0e-9 << std::endl;
   }
 
   return 0;
