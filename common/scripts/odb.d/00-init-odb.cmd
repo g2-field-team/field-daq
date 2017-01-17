8698f83dda67c7455c94ca0ab3b9a1ef00132345
New G2 <newg2@g2nmrdaq1.hep.anl.gov>
2017-01-16 18:43:25 -0600

    better odb init scheme

--- common/scripts/odb.d/00-init-odb.cmd
+++ common/scripts/odb.d/00-init-odb.cmd
@@ -6,8 +6,19 @@ set "/Experiment/Run Parameters/Root Output" n
 create DWORD "/Experiment/MAX_EVENT_SIZE"
 set "/Experiment/MAX_EVENT_SIZE" 0x1000000
 
-mkdir "Logger"
-create BOOL "/Logger/ODB Dump"
-set "Logger/ODB Dump" y
+mkdir "Equipment"
+cd "Equipment"
+load EquipmentInit.xml
 
+cd "/"
+mkdir "Logger"
+cd "Logger"
+load LoggerInit.xml
+
+cd  "/"
+mkdir "Programs"
+cd "Programs"
+load ProgramsInit.xml
+
+cd "/"
 mkdir "Settings"
