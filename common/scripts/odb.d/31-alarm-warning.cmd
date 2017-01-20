1534d70228ca6999d58eefbec0bc402637b5ce82
Matthias W. Smith <mwsmith2@uw.edu>
2017-01-20 11:50:10 -0800

    Updates odb-init files for alarms.

--- common/scripts/odb.d/31-alarm-warning.cmd
+++ common/scripts/odb.d/31-alarm-warning.cmd
@@ -1,6 +1,6 @@
 cd "/Alarms/Classes/Warning"
 
-set "Execute command" "/home/newg2/Applications/field-daq/common/scripts/alarms/alarm_script.sh warning \"%s\""
+set "Execute command" "/home/newg2/Applications/field-daq/online/alarms/alarm_script.sh warning '%s'"
 set "Stop run" n
 set "Display BGColor" "green"
 set "Display FGColor" "yellow"
--- common/scripts/odb.d/33-alarm-failure.cmd
+++ common/scripts/odb.d/33-alarm-failure.cmd
@@ -3,8 +3,7 @@ cd "/Alarms/Classes/"
 cp Alarm Failure
 cd Failure
 
-
-set "Execute command" "/home/newg2/Applications/field-daq/common/scripts/alarms/alarm_script.sh failure \"%s\""
+set "Execute command" "/home/newg2/Applications/field-daq/online/alarms/alarm_script.sh failure '%s'"
 set "Stop run" y
 set "Display BGColor" "red"
 set "Display FGColor" "black"
