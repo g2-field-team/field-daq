b11b75fe30d0bbcb020140b550d281b6b89bb177
Rachel Osofsky <osofskyr@uw.edu>
2017-02-14 07:39:13 -0800

    Simulated python scripts for beaglebone

--- common/src/frontends/src/sim_beagleboneReply.py
+++ common/src/frontends/src/sim_beagleboneReply.py
@@ -13,23 +13,32 @@ context = zmq.Context()
 socket = context.socket(zmq.REP)
 print "Binding to server"
 socket.bind("tcp://*:5555")
+print "Connected successfully"
 
 #Receive the message
+print "About to receive currents"
 string = socket.recv_string()
+print "Received currents"
+print string
+print "Did that work?"
 if string.endswith('\0') : #Get rid of trailing null character 
     string = string[:-1]
-setCurrent1, setCurrent2 = string.split()
+setCurrent1, setCurrent2, setCurrent3, setCurrent4 = string.split()
 
-print("The received values were %s %s" % (setCurrent1, setCurrent2))
+print("The received values were %s %s %s %s" % (setCurrent1, setCurrent2, setCurrent3, setCurrent4))
 
 rand1 = uniform(-0.1,0.1)
 rand2 = uniform(-0.1,0.1)
+rand3 = uniform(-0.1,0.1)
+rand4 = uniform(-0.1,0.1)
 
 currentVal1 = float(setCurrent1) + rand1
 currentVal2 = float(setCurrent2) + rand2
+currentVal3 = float(setCurrent3) + rand3
+currentVal4 = float(setCurrent4) + rand4
 
 #Do some "work"                                                                
 time.sleep(1)
 
 #Send reply back                                                               
-socket.send_string("%f %f\0" % (currentVal1, currentVal2))
+socket.send_string("%f %f %f %f\0" % (currentVal1, currentVal2, currentVal3, currentVal4))
--- common/src/frontends/src/sim_beaglebone_publish.py
+++ common/src/frontends/src/sim_beaglebone_publish.py
@@ -13,10 +13,14 @@ socket = context.socket(zmq.PUB)
 socket.connect("tcp://localhost:5556")
 
 while True:
-    Ch1Current = 2.3 + uniform(-0.1,0.1)
+    Ch1Current = 10.0 + uniform(-0.1,0.1)
     Ch1Temp = 25.0 + uniform(-1.0,1.0)
-    Ch2Current = 4.62 + uniform(-0.1,0.1)
+    Ch2Current = 20.0 + uniform(-0.1,0.1)
     Ch2Temp = 25.0 + uniform(-1.0,1.0)
+    Ch3Current = 0.1 + uniform(-0.1,0.1)
+    Ch3Temp = 25.0 + uniform(-1.0,1.0)
+    Ch4Current = 0.2 + uniform(-0.1,0.1)
+    Ch4Temp = 25.0 + uniform(-1.0,1.0)
     socket.send_string("%f %f %f %f" % (Ch1Current, Ch1Temp, Ch2Current, Ch2Temp))
     print "Sent a message"
     time.sleep(1)
