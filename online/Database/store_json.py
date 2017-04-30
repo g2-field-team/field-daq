ddd8157fb0a576b5fa540768724fe90cc8ae2561
new g-2 <newg2@g2field-be.fnal.gov>
2017-04-30 16:25:20 -0500

    update some scripts

--- online/Database/store_json.py
+++ online/Database/store_json.py
@@ -13,13 +13,13 @@ filename = Data_dir.strip() + 'run' + '{0:05d}'.format(int(Run_number)) + '.json
 
 dbname = sys.argv[1];
 
-run_num =int(filter(str.isdigit,os.path.basename(file_name)));
+run_num =int(filter(str.isdigit,os.path.basename(filename)));
 print(run_num);
 print(dbname);
 print "Pouring Run No.{0} JSON File to Local Database [{1}]...".format(run_num,dbname),
 
-json_text = open(file_name, "r").read();
-conn = psycopg2.connect("host={0} dbname={1} port=5432".format('g2db-priv',dbname));
+json_text = open(filename, "r").read();
+conn = psycopg2.connect("host={0} dbname={1} user={2} port=5432".format('g2db-priv',dbname,'daq'));
 c = conn.cursor();
 c.execute("insert into gm2field_daq(run_num, json_data) values(%s, %s); commit", (run_num, json_text));
 print(' Done!');
--- online/bin/end_run.sh
+++ online/bin/end_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /usr/local/opt/pyenv/bin/activate
 
-#python /home/newg2/Applications/field-daq/online/Database/store_json.py $filename test
-python /home/newg2/Applications/field-daq/online/Database/test.py hhh
+python /home/newg2/Applications/field-daq/online/Database/store_json.py test
+#python /home/newg2/Applications/field-daq/online/Database/test.py hhh
 
