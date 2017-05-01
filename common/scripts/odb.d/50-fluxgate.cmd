c229f07ee2a985701e0ca55c893ceb0133f85bf1
Alec Tewsley-Booth <aetb@umich.edu>
2017-05-01 11:53:40 -0500

    odb init script and fluxgate frontend compatibility

---
+++ common/scripts/odb.d/50-fluxgate.cmd
@@ -0,0 +1,18 @@
+mkdir "/Equipment/Fluxgate/Settings"
+cd "/Equipment/Fluxgate/Settings"
+
+create FLOAT "Minimum Voltage DC"
+set "Minimum Voltage DC" -10.0
+create FLOAT "Maximum Voltage DC"
+set "Minimum Voltage DC" 10.0
+
+create FLOAT "Minimum Voltage AC"
+set "Minimum Voltage DC" -1.0
+create FLOAT "Maximum Voltage AC"
+set "Minimum Voltage DC" 1.0
+
+create FLOAT "Sample Rate"
+set "Sample Rate" 2000.0
+
+create FLOAT "Acquisition Time"
+set "Acquisition Time" 60.0
--- common/src/frontends/src/fluxgate.cxx
+++ common/src/frontends/src/fluxgate.cxx
@@ -11,24 +11,18 @@ About:  Addresses NI PCIe daq card, reads multiple channels of data, performs si
 #include <random>
 #include <sys/time.h>
 #include "midas.h"
-#include "NIDAQmxBase.h" //NI drivers for daq card, add to make file
+#include <NIDAQmxBase.h> //NI drivers for daq card
 #include "frontend_utils.hh"
-#include "field_structs.hh
-#include "field_constants.hh"
+#include "g2field/core/field_constants.hh"
+#include "g2field/core/field_structs.hh"
 using std::string;
 
 //--- globals ---------------------------------------------------------------//
 
-#define FRONTEND_NAME "Fluxgate Frontend" // Prefer capitalize with spaces
+#define FRONTEND_NAME "Fluxgate" // Prefer capitalize with spaces
 const char * const bank_name = "FLUX"; // 4 letters, try to make sensible
 
 // Any structs need to be defined. NEED TO ALTER FOR FLUXGATE
-//
-//FLUXGATE STRUCTS!
-BANK_LIST trigger_bank_list[] = {
-	{bank_name, TID_CHAR, sizeof(g2::point_t), NULL},
-	{""}
-};
 //
 //
 //
@@ -59,7 +53,7 @@ extern "C" {
 	INT read_fluxgate_event(char *pevent, INT off);
 	INT poll_event(INT source, INT count, BOOL test); //needed?
 	INT interrupt_configure(INT cmd, INT source, PTYPE adr);
-	INT ni_error_msg(INT error, char func[], char successmsg[]); //checks for errors and displays msgs as required
+	INT ni_error_msg(INT error, const char *func, const char *successmsg); //checks for errors and displays msgs as required
 
 	//Equipment list for fluxgate, changed from POLLED to PERIODIC
 
@@ -110,21 +104,22 @@ namespace {
 	TaskHandle taskHandle; //NI DAQ task identifier
 	int32 read;
 	char errBuff[2048]={'\0'};
-	int32 numDCChannels = 16;
-	int32 numACChannels = 16;
+	int32 numDCChannels = 12; //these should go into ODB
+	int32 numACChannels = 12; //these should go into ODB
+	int32 numChannels = 24; //these should go into ODB
 	//--- Channel Parameters ----------------------------------------------------//
 	//--- Timing Parameters -----------------------------------------------------//
 	float64 rate = 8000; //8000 samples per second
 	float64 aqTime = 60; //time to acquire in seconds
 	uInt64 sampsPerChanToAcquire = static_cast<uInt64>(rate*aqTime);
-	size_t dataSize = static_cast<size_t>(sampsPerChanToAcquire*numChannels)
+	size_t dataSize = static_cast<size_t>(sampsPerChanToAcquire*numChannels);
 	std::vector<float64> data(dataSize); //vector for writing DAQ buffer into
 	//--- DC Coupled Channels ---------------------------------------------------//
-	const char physicalChannelDC[] = "Dev1/ai0:15"; //creates DC AI channels 0-15
+	const char physicalChannelDC[] = "Dev1/ai0:11"; //creates DC AI channels 0-15
 	float64 minVolDC = -10.0;
 	float64 maxVolDC = 10.0;
 	//--- AC Coupled Channels -----------------------------------------------//
-	const char physicalChannelAC[] = "Dev1/ai16:31"; //creates AC AI channels 16-31
+	const char physicalChannelAC[] = "Dev1/ai12:23"; //creates AC AI channels 16-31
 	float64 minVolAC = -1.0;
 	float64 maxVolAC = 1.0;
 	//
@@ -136,21 +131,21 @@ namespace {
 //
 
 //--- NI Error Check/Message ------------------------------------------------//
-INT ni_error_msg(INT error, char func[], char successmsg[]){
+INT ni_error_msg(INT error, const char *func, const char *successmsg){
 	if( DAQmxFailed(error) ) {
-			DAQmxGetExtendedErrorInfo(errBuff,2048);
-			cm_msg(MERROR, func[], errBuff);
+			DAQmxBaseGetExtendedErrorInfo(errBuff,2048);
+			cm_msg(MERROR, func, errBuff);
 			return 1;
 	}
 	else {
-			cm_msg(MINFO, func[], successmsg[]);
+			cm_msg(MINFO, func, successmsg);
 			return 0;
 	}
 }
 
 //--- Frontend Init ---------------------------------------------------------//
 INT frontend_init() {
-	error = DAQmxCreateTask("",&taskHandle);
+	error = DAQmxBaseCreateTask("",&taskHandle);
 	errorreturn = ni_error_msg(error, "frontend_init", "NI DAQ task created");
 	/*if( DAQmxFailed(error) ) {
 		DAQmxGetExtendedErrorInfo(errBuff,2048);
@@ -164,8 +159,8 @@ INT frontend_init() {
 
 //--- Frontend Exit ---------------------------------------------------------//
 INT frontend_exit(){
-	error = DAQmxClearTask(taskHandle);
-	errorreturn = ni_error_msg(error, "frontend_exit", "NI DAQ task cleared")
+	error = DAQmxBaseClearTask(taskHandle);
+	errorreturn = ni_error_msg(error, "frontend_exit", "NI DAQ task cleared");
 	/*if( DAQmxFailed(error) ) {
 		DAQmxGetExtendedErrorInfo(errBuff,2048);
 		cm_msg(MERROR,"frontend_init",errBuff);
@@ -176,79 +171,74 @@ INT frontend_exit(){
 }
 
 //--- Begin of Run ----------------------------------------------------------//
-INT begin_of_run(INT run_number, char *err)
-{
-	int rc = load_global_settings(global_conf);
-	if (rc != 0) {
-		cm_msg(MERROR, "begin_of_run", "could not load global settings");
-		return rc;
-	}
- 
-	rc = load_settings(frontend_name, global_conf);
+INT begin_of_run(INT run_number, char *err){
+
+	int rc = load_settings(frontend_name, conf);
 	if (rc != 0) {
 		cm_msg(MERROR, "begin_of_run", "could not load equipment settings");
 		return rc;
 	}
 
-	point_min = conf.get<double>("min");
-	point_max = conf.get<double>("max");
-  
+	// ODB parameters
+  	HNDLE hDB, hkey;
+  	char str[256];
+  	int dbsize;
+  	BOOL flag;
+	
+	//get run parameters from ODB
+	cm_get_experiment_database(&hDB, NULL);
+	//
+	//
+	//physical channels are strings of the form "Dev1/ai0:15", THESE MIGHT BE IMPOSSIBLE TO ODB FOR REASONS
+	//dbsize = sizeof(physicalChannelDC);
+	//db_get_value(hDB,0,"/Equipment/Fluxgate/Settings/Physical Channels DC",&physicalChannelDC,&dbsize,TID_STRING, 0);
+	//dbsize = sizeof(physicalChannelAC);
+	//db_get_value(hDB,0,"/Equipment/Fluxgate/Settings/Physical Channels AC",&physicalChannelAC,&dbsize,TID_STRING, 0);
+	//
+	//
+	//DC voltage parameters (nominally -10 to 10 V)
+	dbsize = sizeof(minVolDC);
+	db_get_value(hDB,0,"/Equipment/Fluxgate/Settings/Minimum Voltage DC",&minVolDC,&dbsize,TID_FLOAT, 0);
+	dbsize = sizeof(maxVolDC);
+	db_get_value(hDB,0,"/Equipment/Fluxgate/Settings/Maximum Voltage DC",&maxVolDC,&dbsize,TID_FLOAT, 0);
+	//AC voltage parameters (depends on needed gain)
+	dbsize = sizeof(minVolAC);
+	db_get_value(hDB,0,"/Equipment/Fluxgate/Settings/Minimum Voltage AC",&minVolAC,&dbsize,TID_FLOAT, 0);
+	dbsize = sizeof(maxVolAC);
+	db_get_value(hDB,0,"/Equipment/Fluxgate/Settings/Maximum Voltage AC",&maxVolAC,&dbsize,TID_FLOAT, 0);
+	//timing parameters
+	dbsize = sizeof(rate);
+	db_get_value(hDB,0,"/Equipment/Fluxgate/Settings/Sample Rate",&rate,&dbsize,TID_FLOAT, 0);
+	dbsize = sizeof(aqTime);
+	db_get_value(hDB,0,"/Equipment/Fluxgate/Settings/Acquisition Time",&aqTime,&dbsize,TID_FLOAT, 0);
+	sampsPerChanToAcquire = static_cast<uInt64>(rate*aqTime);
+	  
 	//setup channel and timing parameters
 	//create DC channels
-	error = DAQmxCreateAIVoltageChan(taskHandle,physicalChannelDC[],"Voltage",DAQmx_Val_Cfg_Default,minVolDC,maxVolDC,DAQmx_Val_Volts,NULL);
-	errorreturn = ni_error_msg(error, "begin_of_run", "NI DAQ DC channels configured")
-	/*if( DAQmxFailed(error) ) {
-		DAQmxGetExtendedErrorInfo(errBuff,2048);
-		cm_msg(MERROR,"begin_of_run",errBuff);
-	}
-	else {
-		cm_msg(MINFO,"begin_of_run","fluxgate DC AI channels created");
-	}*/
+	error = DAQmxBaseCreateAIVoltageChan(taskHandle,physicalChannelDC,"Voltage",DAQmx_Val_Cfg_Default,minVolDC,maxVolDC,DAQmx_Val_Volts,NULL);
+	errorreturn = ni_error_msg(error, "begin_of_run", "NI DAQ DC channels configured");
+	
 	//create AC channels
-	error = DAQmxCreateAIVoltageChan(taskHandle,physicalChannelAC[],"Voltage",DAQmx_Val_Cfg_Default,minVolAC,maxVolAC,DAQmx_Val_Volts,NULL);
-	errorreturn = ni_error_msg(error, "begin_of_run", "NI DAQ AC channels configured")
-	/*if( DAQmxFailed(error) ) {
-			DAQmxGetExtendedErrorInfo(errBuff,2048);
-			cm_msg(MERROR,"begin_of_run",errBuff);
-	}
-	else {
-			cm_msg(MINFO,"begin_of_run","fluxgate AC AI channels created");
-	}*/
+	error = DAQmxBaseCreateAIVoltageChan(taskHandle,physicalChannelAC,"Voltage",DAQmx_Val_Cfg_Default,minVolAC,maxVolAC,DAQmx_Val_Volts,NULL);
+	errorreturn = ni_error_msg(error, "begin_of_run", "NI DAQ AC channels configured");
+	
 	//setup timing
-	error = DAQmxCfgSampClkTiming(taskHandle,"",rate,DAQmx_Val_Rising,DAQmx_Val_ContSamps,sampsPerChanToAcquire);
-	errorreturn = ni_error_msg(error, "begin_of_run", "NI DAQ timing configured")
-	/*if( DAQmxFailed(error) ) {
-			DAQmxGetExtendedErrorInfo(errBuff,2048);
-			cm_msg(MERROR,"begin_of_run",errBuff);
-	}
-	else {
-			cm_msg(MINFO,"begin_of_run","fluxgate daq timing set");
-	}*/
+	error = DAQmxBaseCfgSampClkTiming(taskHandle,"",rate,DAQmx_Val_Rising,DAQmx_Val_ContSamps,sampsPerChanToAcquire);
+	errorreturn = ni_error_msg(error, "begin_of_run", "NI DAQ timing configured");
+	
 	//start task
-	error = DAQmxStartTask(taskHandle);
-	errorreturn = ni_error_msg(error, "begin_of_run", "NI DAQ task started")
-	/*if( DAQmxFailed(error) ) {
-			DAQmxGetExtendedErrorInfo(errBuff,2048);
-			cm_msg(MERROR,"begin_of_run",errBuff);
-	}
-	else {
-			cm_msg(MINFO,"begin_of_run","fluxgate daq task started");
-	}*/
+	error = DAQmxBaseStartTask(taskHandle);
+	errorreturn = ni_error_msg(error, "begin_of_run", "NI DAQ task started");
+	
 	return SUCCESS;
 }
 
 //--- End of Run ------------------------------------------------------------//
 INT end_of_run(INT run_number, char *err){
 	//stop task
-	error = DAQmxStopTask(taskHandle);
-	errorreturn = ni_error_msg(error, "end_of_run", "NI DAQ task stopped")
-	/*if( DAQmxFailed(error) ) {
-			DAQmxGetExtendedErrorInfo(errBuff,2048);
-			cm_msg(MERROR,"end_of_run",errBuff);
-	}
-	else {
-			cm_msg(MINFO,"end_of_run","DAQ task stopped");;
-	}*/
+	error = DAQmxBaseStopTask(taskHandle);
+	errorreturn = ni_error_msg(error, "end_of_run", "NI DAQ task stopped");
+	
 	return SUCCESS;
 }
 
@@ -272,7 +262,7 @@ INT frontend_loop(){
 /****************************************************************************\
 	Readout routines for different events
 \*****************************************************************************/
-INT poll_event(INT source, INT count, BOOL test) {
+INT poll_event(INT source, INT count, BOOL test) { //do I even need this, since it is periodic?
 	static unsigned int i;
 
 	// fake calibration
@@ -281,18 +271,8 @@ INT poll_event(INT source, INT count, BOOL test) {
 			usleep(10);
 		}
 
-		last_event = steadyclock_us();
-
 		return 0;
 	}
-	next_event = steadyclock_us();
-	// Check hardware for events, just random here.
-	if (next_event - last_event > 100000) {
-		return 1;
-	}
-	else {
-		return 0;
-	}	
 }
 
 //--- Interrupt configuration ---------------------------------------*/
@@ -313,26 +293,20 @@ INT interrupt_configure(INT cmd, INT source, PTYPE adr){
 //--- Event readout -------------------------------------------------*/
 INT read_fluxgate_event(char *pevent, INT off){
 	BYTE *pdata;
-	WORD *pfluxdata
+	WORD *pfluxdata;
 	// Let MIDAS allocate the struct.
-	bk_create(pevent, bank_name, TID_BYTE, &pdata);
+	bk_create(pevent, bank_name, TID_BYTE, (void**)&pdata);
 
 	// Grab a timestamp.
 	//last_event = steadyclock_us();
 
-	// Fill the struct.
-	//point.timestamp = last_event;
-	//point.x = (double)rand() / RAND_MAX * (point_max - point_min) + point_min;
-	//point.y = (double)rand() / RAND_MAX * (point_max - point_min) + point_min;
-	//point.z = (double)rand() / RAND_MAX * (point_max - point_min) + point_min;
-
 	// And MIDAS output.
 	bk_init32(pevent);
 	// Copy the fluxgate data.
-	bk_create(pevent, bank_name, TID_WORD, &pfluxdata);
 	memcpy(pfluxdata, &data, sizeof(data));
 	pdata += sizeof(data) / sizeof(WORD);
 	bk_close(pevent, pdata);
   
 	return bk_size(pevent);
 }
+
