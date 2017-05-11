f05061b2ad8b943c9c56dbe3be7bf2686ca6ff03
Rachel Osofsky <osofskyr@uw.edu>
2017-05-11 07:37:13 -0700

    Changed surface coil root directory location in ODB

--- common/scripts/odb.d/44-surface-coils.cmd
+++ common/scripts/odb.d/44-surface-coils.cmd
@@ -1,7 +1,7 @@
 cd "/Equipment/Surface Coils/Settings"
 create BOOL "Root Output"
 set "Root Output" false
-create STRING "Root Dir[1][256]"
+create STRING "Root Directory[1][256]"
 set "Root Dir" "/home/newg2/Applications/field-daq/resources/"
 
 cd
