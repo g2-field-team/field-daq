936145811d585e6cad76fd38cf5ea3caf855147e
Alec Tewsley-Booth <aetb@umich.edu>
2017-05-23 10:42:11 -0500

    added fluxgate_utils.hh, streamlined fluxgate.cxx, testing macro-based NI error messages

---
+++ common/src/frontends/include/fluxgate_utils.hh
@@ -0,0 +1,66 @@
+#ifndef FRONTENDS_INCLUDE_FLUXGATE_UTILS_HH_
+#define FRONTENDS_INCLUDE_FLUXGATE_UTILS_HH_
+
+/*=================================================================================*\
+
+author: Alec Tewsley-Booth
+email:  aetb@umich.edu
+file:   fluxgate_utils.hh
+about:  Contains parameters, structures, and helper functions for fluxgate frontend.
+
+\*=================================================================================*/
+
+//--- std includes ----------------------------------------------------------//
+//--- other includes --------------------------------------------------------//
+#include <NIDAQmxBase.h>
+#include "TROOT.h"
+#include "g2field/core/field_constants.hh"
+#include "g2field/core/field_structs.hh"
+
+/*======================"Hidden" parameters --- timing/array sizes, mostly ===================================*/
+/*----------------------------------- acquisition parameters -------------------------------------------------*/
+//SET parameters
+#define AQ_RATE 2000 // acquisition rate in samples per second per channel (MAKE AN INT!!!)
+#define AQ_TIME 10 // acquisiton time in seconds (MAKE AN INT!!!)
+#define AQ_TOTALCHAN 24 // total channels, including AC and DC channels
+//DERIVED paramters
+#define AQ_TIMEMS AQ_TIME*1000 // acquisition time in msec
+#define AQ_SAMPSPERCHAN AQ_RATE*AQ_TIME // number of samples to aquire per channel, equals AQ_RATE x AQ_TIME
+#define AQ_TOTALSAMPS AQ_SAMPSPERCHAN*AQ_TOTALCHAN // total number of samples, equals AQ_SAMPSPERCHAN x AQ_TOTALCHAN
+/*------------------------------------------------------------------------------------------------------------*/
+/*----------------------------------- filter parameters ------------------------------------------------------*/
+#define FILT_ROLLOFF 1000 // digital lowpass rolloff in Hz
+#define FILT_BINSIZE 1 // bin size for binning and averaging routine, note rate/binsize should be >= 2x filter rolloff
+/*------------------------------------------------------------------------------------------------------------*/
+/*============================================================================================================*/
+
+namespace g2field {
+//fluxgate struct
+	struct fluxgate_t{
+		Double_t sys_clock;
+		Double_t gps_clock;
+		Float_t fg_r[8];
+		Float_t fg_theta[8];
+		Float_t fg_z[8];
+		float64 data[AQ_TOTALSAMPS];
+	};
+	#define MAKE_FG_STRING(name) FG_HELPER(name)
+	#define FG_HELPER(name) const char * const name = "sys_clock/D:gps_clock/D:fg_r[8]/F:fg_theta[8]/F:fg_z[8]/F:data[AQ_TOTALSAMPS]/D"
+	MAKE_FG_STRING(fg_str);
+	
+	constexpr float64 kFluxRate = AQ_RATE;
+	constexpr float64 kFluxTime = AQ_TIME;
+	constexpr int32 kFluxNumChannels = AQ_TOTALCHAN;
+	constexpr uInt64 kFluxSampsPerChanToAcquire = AQ_SAMPSPERCHAN;
+	constexpr uInt32 kFluxArraySizeInSamps = AQ_TOTALSAMPS;
+	
+} // ::g2field
+
+
+
+
+
+
+
+
+#endif
--- common/src/frontends/src/fluxgate.cxx
+++ common/src/frontends/src/fluxgate.cxx
@@ -2,42 +2,29 @@
 Name:   fluxgateFe.cxx
 Author: Alec Tewsley-Booth
 Email:  aetb@umich.edu
-About:  Addresses NI PCIe daq card, reads multiple channels of data, performs simple manipulations on the data, saves a header and .mid file.        
+About:  Addresses NI PCIe daq card, reads multiple channels of data, performs simple manipulations on the data, saves a .mid file.        
 \*****************************************************************************/
 #include <unistd.h>
 #include <stdio.h>
 #include <iostream>
 #include <stdlib.h>
 #include <string>
-#include <random>
 #include <sys/time.h>
 #include "midas.h"
 #include <NIDAQmxBase.h> //NI drivers for daq card
 #include "frontend_utils.hh"
+#include "fluxgate_utils.hh"
 #include "g2field/core/field_constants.hh"
 #include "g2field/core/field_structs.hh"
 using std::string;
-using namespace std;
 
-//---Run parameters that I don't want editable through the ODB --------------//
-// acquisition parameters
-#define AQ_RATE 2000 // acquisition rate in samples per second per channel
-#define AQ_TIME 10 // acquisiton time in seconds
-#define AQ_TIMEMS 10000 // acquisition time in msec
-#define AQ_SAMPSPERCHAN 120000 // number of samples to aquire per channel, equals AQ_RATE x AQ_TIME
-#define AQ_TOTALCHAN 24 // total channels, including AC and DC channels
-#define AQ_TOTALSAMPS 2880000 // total number of samples, equals AQ_SAMPSPERCHAN x AQ_TOTALCHAN
-// filter parameters
-#define FILT_ROLLOFF 1000 // digital lowpass rolloff in Hz
-#define FILT_BINSIZE 1 // bin size for binning and averaging routine, note rate/binsize should be >= 2x filter rolloff
+// Run parameters that I don't want editable through the ODB are in frontends/include/fluxgate_utils.hh
 
-//--- globals ---------------------------------------------------------------//
 
+//====================== BEGIN GLOBALS =========================================================//
 #define FRONTEND_NAME "Fluxgate" // Prefer capitalize with spaces
 const char * const bank_name = "FLUX"; // 4 letters, try to make sensible
-
 // Any structs need to be defined.
-
 extern "C" {
 	// The frontend name (client name) as seen by other MIDAS clients
 	char *frontend_name = (char*)FRONTEND_NAME;
@@ -64,116 +51,101 @@ extern "C" {
 	INT read_fluxgate_event(char *pevent, INT off);
 	INT poll_event(INT source, INT count, BOOL test); //needed?
 	INT interrupt_configure(INT cmd, INT source, PTYPE adr);
-	INT ni_error_msg(INT error, const char *func, const char *successmsg); //checks for errors and displays msgs as required
-
-	//Equipment list for fluxgate, changed from POLLED to PERIODIC
-
-	EQUIPMENT equipment[] = 
-    {
+	EQUIPMENT equipment[] = {
 		{FRONTEND_NAME,   // equipment name 
-		{10, 0,         // event ID, trigger mask (need to figure these out)
-        "SYSTEM",      // event buffer (used to be SYSTEM)
-        EQ_PERIODIC,     // equipment type 
-        0,             // event source, not used 
-        "MIDAS",       // format 
-        TRUE,          // enabled 
-        RO_RUNNING,    // read only when running 
-        10000,           // read every 10 seconds 
-        0,             // stop run after this event limit 
-        0,             // number of sub events 
-        1,             // don't log history 
-        "", "", "",
-		},
+			{10, 0,         // event ID, trigger mask (need to figure these out)
+        		"SYSTEM",      // event buffer (used to be SYSTEM)
+        		EQ_PERIODIC,     // equipment type 
+        		0,             // event source, not used 
+        		"MIDAS",       // format 
+        		TRUE,          // enabled 
+        		RO_RUNNING,    // read only when running 
+        		AQ_TIMEMS,           // read every 10 seconds 
+       			0,             // stop run after this event limit 
+        		0,             // number of sub events 
+        		1,             // don't log history 
+        		"", "", "",},
 		read_fluxgate_event,      // readout routine 
 		NULL,
 		NULL,
-		NULL, 
-		},
+		NULL,},
 		{""}
-    };
+	}; //end equipment
 } //end extern "C"
-
 // Put necessary globals in an anonymous namespace here.
 namespace {
-	boost::property_tree::ptree conf;
-	boost::property_tree::ptree global_conf;
-	double point_min = 0.0;
-	double point_max = 1.0;
-	long long last_event;
-	long long next_event;
-	//
-	//
-	//THESE SHOULD BE SWAPPED TO ODB PARAMETERS WHERE POSSIBLE
-	//REMOVE OUTDATED MACROES, SWITCH TO ODB --- MINIMIZE RECOMPILING
 	//--- NI DAQ globals --------------------------------------------------------//
-	int32 DAQerr = 0; //DAQ tasks
-	int errorreturn = 0; //error check/message returns
+	int32 DAQerr; //DAQ tasks
 	TaskHandle taskHandle; //NI DAQ task identifier
-	int32 read;
 	char errBuff[2048]={'\0'};
-	int32 numDCChannels = 12; //these should go into ODB
-	int32 numACChannels = 12; //these should go into ODB
-	int32 numChannels = 24; //these should go into ODB
 	//--- Channel Parameters ----------------------------------------------------//
-	//--- Timing Parameters -----------------------------------------------------//
-	float64 rate = AQ_RATE;
-	float64 aqTime = AQ_TIME;
-	uInt64 sampsPerChanToAcquire = AQ_SAMPSPERCHAN;
-	float64 data[AQ_TOTALSAMPS]; //vector for writing DAQ buffer into, fixing runtime allocation for testing
-	//--- DC Coupled Channels ---------------------------------------------------//
-	const char *physicalChannelDC = "Dev1/ai0:11"; //creates DC AI channels 0-15
+	int32 numChannels = g2field::kFluxNumChannels; //defined in fluxgate_utils.hh
+	const char *physicalChannelDC = "Dev1/ai0:11"; //creates DC AI channels 0-11
 	float64 minVolDC = -10.0;
 	float64 maxVolDC = 10.0;
-	//--- AC Coupled Channels -----------------------------------------------//
-	const char *physicalChannelAC = "Dev1/ai12:23"; //creates AC AI channels 16-31
+	const char *physicalChannelAC = "Dev1/ai12:23"; //creates AC AI channels 12-23
 	float64 minVolAC = -1.0;
 	float64 maxVolAC = 1.0;
+	//--- Timing Parameters -----------------------------------------------------//
+	float64 rate = g2field::kFluxRate;
+	float64 aqTime = g2field::kFluxTime;
+	uInt64 sampsPerChanToAcquire = AQ_SAMPSPERCHAN;
 	//--- Readout Parameters ---------------------------------------//
-	uInt32 arraySizeInSamps = sampsPerChanToAcquire*numChannels;
+	uInt32 arraySizeInSamps = AQ_TOTALSAMPS;
+	float64 readOut[AQ_TOTALSAMPS]; //vector for writing DAQ buffer into, fixing runtime allocation for testing
 	int32 sampsRead = 0;
+	g2field::fluxgate_t data; //fluxgate data type defined in fluxgate_utils.hh
 	//--- ODB Parameters --------------------------------------------//
 	HNDLE hDB, hkey;
-	//
-	//
 } //end anonymous namespace
-//
-//
-//
+//====================== END GLOBALS ===========================================================//
+
+//====================== MACROS ================================================================//
+#define NIERRORCHECK_SUCCESSMSG(func,errormsg,successmsg) \
+	if( DAQmxFailed(DAQerr) ) {\
+		DAQmxBaseGetExtendedErrorInfo(errBuff,2048);\
+		cm_msg(MERROR,func,errormsg);\
+		cm_msg(MERROR,func,errBuff);}\
+	else {cm_msg(MINFO,func,successmsg);}
+#define NIERRORCHECK_NOSUCCESSMSG(func,errormsg) \
+	if( DAQmxFailed(DAQerr) ) {\
+		DAQmxBaseGetExtendedErrorInfo(errBuff,2048);\
+		cm_msg(MERROR,func,errormsg);\
+		cm_msg(MERROR,func,errBuff);}
 
 //--- Frontend Init ---------------------------------------------------------//
 INT frontend_init() {
 	DAQerr = DAQmxBaseCreateTask("",&taskHandle);
-	if( DAQmxFailed(DAQerr) ) {
+	NIERRORCHECK_SUCCESSMSG("frontend_init","error creating fluxgate task","fluxgate task created");
+/*	if( DAQmxFailed(DAQerr) ) {
 		DAQmxBaseGetExtendedErrorInfo(errBuff,2048);
 		cm_msg(MERROR,"frontend_init",errBuff);
 	}
 	else {
 		cm_msg(MINFO,"frontend_init","fluxgate DAQ task created");
 	}
+*/
 	return SUCCESS;
 }
 
 //--- Frontend Exit ---------------------------------------------------------//
 INT frontend_exit(){
 	DAQerr = DAQmxBaseClearTask(taskHandle);
+	NIERRORCHECK_SUCCESSMSG("frontend_exit","error clearing fluxgate task","fluxgate task cleared");
+/*
 	if( DAQmxFailed(DAQerr) ) {
 		DAQmxBaseGetExtendedErrorInfo(errBuff,2048);
 		cm_msg(MERROR,"frontend_init",errBuff);
 	} else {
 		cm_msg(MINFO,"frontend_exit","fluxgate DAQ task cleared");
 	}
+*/
 	return SUCCESS;
 }
 
 //--- Begin of Run ----------------------------------------------------------//
 INT begin_of_run(INT run_number, char *err){
-/*
-	int rc = load_settings(frontend_name, conf);
-	if (rc != 0) {
-		cm_msg(MERROR, "begin_of_run", "could not load equipment settings");
-		return rc;
-	}
-*/
+
 	// ODB parameters
   	HNDLE hDB, hkey;
   	char str[256];
@@ -182,35 +154,23 @@ INT begin_of_run(INT run_number, char *err){
 	
 	//get run parameters from ODB
 	cm_get_experiment_database(&hDB, NULL);
-	//
-	//
-	//physical channels are strings of the form "Dev1/ai0:15", THESE MIGHT BE IMPOSSIBLE TO ODB FOR REASONS
-	//dbsize = sizeof(physicalChannelDC);
-	//db_get_value(hDB,0,"/Equipment/Fluxgate/Settings/Physical Channels DC",&physicalChannelDC,&dbsize,TID_STRING, 0);
-	//dbsize = sizeof(physicalChannelAC);
-	//db_get_value(hDB,0,"/Equipment/Fluxgate/Settings/Physical Channels AC",&physicalChannelAC,&dbsize,TID_STRING, 0);
-	//
-	//
+	
 	//DC voltage parameters (nominally -10 to 10 V)
-	dbsize = sizeof(minVolDC);
+	dbsize = sizeof(minVolDC); //size of float64
 	db_get_value(hDB,0,"/Equipment/Fluxgate/Settings/Minimum Voltage DC",&minVolDC,&dbsize,TID_FLOAT, 0);
-	dbsize = sizeof(maxVolDC);
 	db_get_value(hDB,0,"/Equipment/Fluxgate/Settings/Maximum Voltage DC",&maxVolDC,&dbsize,TID_FLOAT, 0);
-	//AC voltage parameters (depends on needed gain)
-	dbsize = sizeof(minVolAC);
+	//AC voltage parameters (depends on needed gain), still float64
 	db_get_value(hDB,0,"/Equipment/Fluxgate/Settings/Minimum Voltage AC",&minVolAC,&dbsize,TID_FLOAT, 0);
-	dbsize = sizeof(maxVolAC);
 	db_get_value(hDB,0,"/Equipment/Fluxgate/Settings/Maximum Voltage AC",&maxVolAC,&dbsize,TID_FLOAT, 0);
-	//timing parameters
-	dbsize = sizeof(rate);
+	//timing parameters, still float64
 	db_get_value(hDB,0,"/Equipment/Fluxgate/Settings/Sample Rate",&rate,&dbsize,TID_FLOAT, 0);
-	dbsize = sizeof(aqTime);
 	db_get_value(hDB,0,"/Equipment/Fluxgate/Settings/Acquisition Time",&aqTime,&dbsize,TID_FLOAT, 0);
-	sampsPerChanToAcquire = static_cast<uInt64>(rate*aqTime);
 	  
 	//setup channel and timing parameters
 	//create DC channels
 	DAQerr = DAQmxBaseCreateAIVoltageChan(taskHandle,physicalChannelDC,"Voltage",DAQmx_Val_Cfg_Default,-10,10,DAQmx_Val_Volts,NULL);
+	NIERRORCHECK_SUCCESSMSG("begin_of_run","error creating fluxgate DC channels","fluxgate DC channels created");
+/*
 	if( DAQmxFailed(DAQerr) ) {
 		DAQmxBaseGetExtendedErrorInfo(errBuff,2048);
 		cm_msg(MERROR,"begin_of_run","error creating fluxgate DC channels");
@@ -219,9 +179,11 @@ INT begin_of_run(INT run_number, char *err){
 	else {
 		cm_msg(MINFO,"begin_of_run","fluxgate DC channels created");
 	}
-	
+*/	
 	//create AC channels
-	DAQerr = DAQmxBaseCreateAIVoltageChan(taskHandle,physicalChannelAC,"Voltage",DAQmx_Val_Cfg_Default,minVolAC,maxVolAC,DAQmx_Val_Volts,NULL);
+	DAQerr = DAQmxBaseCreateAIVoltageChan(taskHandle, physicalChannelAC, "Voltage", DAQmx_Val_Cfg_Default, minVolAC, maxVolAC, DAQmx_Val_Volts, NULL);
+	NIERRORCHECK_SUCCESSMSG("begin_of_run","error creating fluxgate AC channels","fluxgate AC channels created");
+/*
 	if( DAQmxFailed(DAQerr) ) {
 		DAQmxBaseGetExtendedErrorInfo(errBuff,2048);
 		cm_msg(MERROR,"begin_of_run","error creating fluxgate AC channels");
@@ -230,9 +192,11 @@ INT begin_of_run(INT run_number, char *err){
 	else {
 		cm_msg(MINFO,"begin_of_run","fluxgate AC channels created");
 	}
-	
+*/	
 	//setup timing
-	DAQerr = DAQmxBaseCfgSampClkTiming(taskHandle,"",rate,DAQmx_Val_Rising,DAQmx_Val_FiniteSamps,sampsPerChanToAcquire);
+	DAQerr = DAQmxBaseCfgSampClkTiming(taskHandle, "", AQ_RATE, DAQmx_Val_Rising, DAQmx_Val_FiniteSamps, AQ_SAMPSPERCHAN);
+	NIERRORCHECK_SUCCESSMSG("begin_of_run","error configuring fluxgate sample clock","fluxgate sample clock configured");
+/*
 	if( DAQmxFailed(DAQerr) ) {
 		DAQmxBaseGetExtendedErrorInfo(errBuff,2048);
 		cm_msg(MERROR,"begin_of_run","error configuring fluxgate sample clock");
@@ -241,9 +205,11 @@ INT begin_of_run(INT run_number, char *err){
 	else {
 		cm_msg(MINFO,"begin_of_run","fluxgate sample clock configured");
 	}
-	
+*/
 	//start task
 	DAQerr = DAQmxBaseStartTask(taskHandle);
+	NIERRORCHECK_NOSUCCESSMSG("begin_of_run","error starting fluxgate task");
+/*
 	if( DAQmxFailed(DAQerr) ) {
 		DAQmxBaseGetExtendedErrorInfo(errBuff,2048);
 
@@ -253,7 +219,7 @@ INT begin_of_run(INT run_number, char *err){
 	else {
 		cm_msg(MINFO,"begin_of_run","fluxgate task started");
 	}
-	
+*/
 	return SUCCESS;
 }
 
@@ -261,6 +227,8 @@ INT begin_of_run(INT run_number, char *err){
 INT end_of_run(INT run_number, char *err){
 	//stop task
 	DAQerr = DAQmxBaseStopTask(taskHandle);
+	NIERRORCHECK_NOSUCCESSMSG("end_of_run","error stopping fluxgate task");
+/*
 	if( DAQmxFailed(DAQerr) ) {
 		DAQmxBaseGetExtendedErrorInfo(errBuff,2048);
 
@@ -270,7 +238,7 @@ INT end_of_run(INT run_number, char *err){
 	else {
 		cm_msg(MINFO,"end_of_run","fluxgate task stopped");
 	}
-	
+*/
 	return SUCCESS;
 }
 
@@ -327,11 +295,11 @@ INT read_fluxgate_event(char *pevent, INT off){
 	BYTE *pdata;
 	WORD *pfluxdata;
 	// Let MIDAS allocate the struct.
-
-
 	//setup nidaq acquisition
 	//DAQerr = DAQmxBaseReadAnalogF64(taskHandle,numSampsPerChan,timeout,fillMode,data,arraySizeInSamps,&sampsRead,NULL);
-	DAQerr = DAQmxBaseReadAnalogF64(taskHandle,AQ_SAMPSPERCHAN,AQ_TIME + 5,DAQmx_Val_GroupByChannel,data,AQ_TOTALSAMPS,&sampsRead,NULL);
+	DAQerr = DAQmxBaseReadAnalogF64(taskHandle, sampsPerChanToAcquire, aqTime + 5, DAQmx_Val_GroupByChannel, readOut, arraySizeInSamps, &sampsRead, NULL);	
+	NIERRORCHECK_NOSUCCESSMSG("read_fluxgate_event","error reading fluxgate event");
+/*
 	if( DAQmxFailed(DAQerr) ) {
 		DAQmxBaseGetExtendedErrorInfo(errBuff,2048);
 		cm_msg(MERROR,"read_fluxgate_event","error reading fluxgate event");
@@ -339,22 +307,28 @@ INT read_fluxgate_event(char *pevent, INT off){
 	} else {
 		cm_msg(MINFO,"read_fluxgate_event","fluxgate DAQ event read");
 	}
+*/
 	//restart DAQ task
 	DAQerr = DAQmxBaseStopTask(taskHandle);
+	NIERRORCHECK_NOSUCCESSMSG("read_fluxgate_event","error restarting (stop) fluxgate task");
+/*
 	if( DAQmxFailed(DAQerr) ) {
 		DAQmxBaseGetExtendedErrorInfo(errBuff,2048);
 
 		cm_msg(MERROR,"read_fluxgate_event","error restarting (stop) fluxgate task");
 		cm_msg(MERROR,"read_fluxgate_event",errBuff);
 	}
+*/
 	DAQerr = DAQmxBaseStartTask(taskHandle);
+	NIERRORCHECK_NOSUCCESSMSG("read_fluxgate_event","error restarting (start) fluxgate task");
+/*
 	if( DAQmxFailed(DAQerr) ) {
 		DAQmxBaseGetExtendedErrorInfo(errBuff,2048);
 
 		cm_msg(MERROR,"read_fluxgate_event","error restarting (start) fluxgate task");
 		cm_msg(MERROR,"read_fluxgate_event",errBuff);
 	}
-
+*/
 
 	// And MIDAS output.
 /*
