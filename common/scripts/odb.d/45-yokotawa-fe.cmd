4ee1fa523e7e7061f318e9e2cadcc488df0f141f
Muon g-2 <newg2@g2field-fe2.fnal.gov>
2017-05-22 21:38:55 -0500

    Updates to the Yokogawa frontend; code for PID in place (needs checking)

--- common/scripts/odb.d/45-yokotawa-fe.cmd
+++ common/scripts/odb.d/45-yokotawa-fe.cmd
@@ -13,12 +13,20 @@ create BOOL "Simulation Mode"
 set "Simulation Mode" false
 create BOOL "Feedback Active"
 set "Feedback Active" false
-create DOUBLE "Current Setpoint"
-set "Current Setpoint" 0.000
+create DOUBLE "Current Setpoint (mA)"
+set "Current Setpoint (mA)" 0.000
 create DOUBLE "Field Setpoint"
 set "Field Setpoint" 0.000
 create DOUBLE "Field Readout Value"
 set "Field Readout Value" 0.000
+create DOUBLE "P Coefficient"
+set "P Coefficient" 0.000
+create DOUBLE "I Coefficient"
+set "I Coefficient" 0.000
+create DOUBLE "D Coefficient"
+set "D Coefficient" 0.000
+create DOUBLE "Scale Factor (Amps/Hz)"
+set "Scale Factor (Amps/Hz)" 1.000
 
 cd "/Equipment/Yokogawa/Monitors"
 
@@ -28,5 +36,5 @@ create BOOL "Read Thread Active"
 set "Read Thread Active" false
 create DOUBLE "Average Field"
 set "Average Field" 0.00
-create DOUBLE "Current Value"
-set "Current Value" 0.00
+create DOUBLE "Current Value (mA)"
+set "Current Value (mA)" 0.00
