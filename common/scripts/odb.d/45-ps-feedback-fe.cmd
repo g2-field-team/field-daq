4a865690b5591fb70ef46c6c69da1af1e11ab464
new g-2 <newg2@g2field-be.fnal.gov>
2017-07-05 11:44:36 -0500

    Update to the PS feedback ODB tree

--- common/scripts/odb.d/45-ps-feedback-fe.cmd
+++ common/scripts/odb.d/45-ps-feedback-fe.cmd
@@ -28,7 +28,7 @@ set "D Coefficient" 0.000
 create DOUBLE "I Alt Coefficient"
 set "I Alt Coefficient" 0.000
 create DOUBLE "Scale Factor (kHz per mA)"
-set "Scale Factor (kHz per mA)" 1.000
+set "Scale Factor (kHz per mA)" 0.246 
 create BOOL "Use Single Probe for Field Avg"
 set "Use Single Probe for Field Avg" false
 create INT "Probe Number for Field Avg"
@@ -37,6 +37,10 @@ create DOUBLE "Feedback Threshold (Hz)"
 set "Feedback Threshold (Hz)" 100.00 
 create DOUBLE "Maximum Correction Size (Hz)"
 set "Maximum Correction Size (Hz)" 1.50
+create DOUBLE "Maximum PID Output (Hz)"
+set "Maximum PID Output (Hz)" 100.00
+create DOUBLE "Maximum I Term Output (Hz)"
+set "Maximum I Term Output (Hz)" 100.00
 
 cd "/Equipment/PS Feedback/Monitors"
 
